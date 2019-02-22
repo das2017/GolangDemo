05ead14ae08e7a773d82df625fe327d879d4ae1d
中小研发团队架构实践@新书热销 <38510430@qq.com>
2019-02-22 18:44:59 +0800

    Update main.go

--- webDemo/main.go
+++ webDemo/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/golangDemo/webDemo/models"
+	"github.com/GolangDemo/webDemo/models"
 
 	"github.com/astaxie/beego"
-	_ "github.com/golangDemo/webDemo/routers"
+	_ "github.com/GolangDemo/webDemo/routers"
 )
 
 func main() {
